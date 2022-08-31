c8edc54ae59eadf6e7c158c99f87aa10514395fb
zy_H <hzy18023@gmail.com>
2022-08-31 18:25:19 +0900

    feat: [118]杨辉三角

--- editor/cn/[118]杨辉三角.ts
+++ editor/cn/[118]杨辉三角.ts
@@ -33,6 +33,21 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 function generate(numRows: number): number[][] {
-
+    if (numRows === 1) return [[1]]
+    if (numRows === 2) return [[1], [1, 1]]
+    if (numRows === 3) return [[1], [1, 1], [1, 2, 1]]
+    const res: number[][] = [[1], [1, 1], [1, 2, 1]]
+    for (let row = 3; row < numRows; row++) {
+        res[row] = new Array(row + 1).fill(0)
+        res[row][0] = 1
+        res[row][row] = 1
+        for (let i = 1; i < row; i++) {
+            res[row][i] = res[row - 1][i - 1] + res[row - 1][i]
+        }
+    }
+    return res
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
+
+console.log(generate(5))
