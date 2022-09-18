296d0c43884a1ef327972b84bfaf6b924bde02f6
zy_H <hzy18023@gmail.com>
2022-09-18 21:40:57 +0900

    feat: [剑指 offer 06]

---
+++ editor/cn/[剑指 Offer 06]从尾到头打印链表.ts
@@ -0,0 +1,42 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+//
+// Related Topics 栈 递归 链表 双指针 👍 334 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reversePrint(head: ListNode | null): number[] {
+    const res = []
+    let node = head
+    while (node !== null) {
+        res.unshift(node.val)
+        node = node.next
+    }
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
