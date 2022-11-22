ebfca2d9db59f4c11d95f290ae7fe6f4be33d09c
zyH <hzy18023@gmail.com>
2022-11-22 16:15:32 +0800

    feat: [剑指 Offer 32 - II]

---
+++ editor/cn/[剑指 Offer 32 - II]从上到下打印二叉树 II.ts
@@ -0,0 +1,72 @@
+//从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。 
+//
+// 
+//
+// 例如: 给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+//
+// 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-
+//traversal/ 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 256 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+    if (root === null) return []
+    const res: number[][] = []
+    
+    const nodeQueue = [root]
+    while (nodeQueue.length !== 0) {
+        const temp = []
+        const len = nodeQueue.length
+        for (let i = 0; i < len; i++) {
+            const node = nodeQueue.shift()
+            temp.push(node.val)
+            node.left && nodeQueue.push(node.left)
+            node.right && nodeQueue.push(node.right)
+        }
+        res.push(temp)
+    }
+    
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
