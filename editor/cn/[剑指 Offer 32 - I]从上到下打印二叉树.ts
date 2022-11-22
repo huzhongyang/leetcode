20f8a50be93f2f4bd6700b42f01257dd62e1596c
zyH <hzy18023@gmail.com>
2022-11-22 16:20:44 +0800

    feat: [剑指 Offer 32 - I]

---
+++ editor/cn/[剑指 Offer 32 - I]从上到下打印二叉树.ts
@@ -0,0 +1,61 @@
+//从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。 
+//
+// 
+//
+// 例如: 给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回： 
+//
+// [3,9,20,15,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 237 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[] {
+    if (root === null) return []
+    const nodeQueue = [root]
+    const res = []
+    while (nodeQueue.length !== 0) {
+        const len = nodeQueue.length
+        for (let i = 0; i < len; i++) {
+            const node = nodeQueue.shift()
+            res.push(node.val)
+            node.left && nodeQueue.push(node.left)
+            node.right && nodeQueue.push(node.right)
+        }
+    }
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
