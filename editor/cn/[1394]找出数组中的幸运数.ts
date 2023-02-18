b5ae7198512257a3dd3c0bff2029346c7a138041
zyH <hzy18023@gmail.com>
2023-02-18 16:51:48 +0800

    feat: [1394]

---
+++ editor/cn/[1394]找出数组中的幸运数.ts
@@ -0,0 +1,89 @@
+/**
+ 在整数数组中，如果一个整数的出现频次和它的数值大小相等，我们就称这个整数为「幸运数」。
+ 
+ 给你一个整数数组 arr，请你从中找出并返回一个幸运数。
+ 
+ 
+ 如果数组中存在多个幸运数，只需返回 最大 的那个。
+ 如果数组中不含幸运数，则返回 -1 。
+ 
+ 
+ 
+ 
+ 示例 1：
+ 
+ 输入：arr = [2,2,3,4]
+ 输出：2
+ 解释：数组中唯一的幸运数是 2 ，因为数值 2 的出现频次也是 2 。
+ 
+ 
+ 示例 2：
+ 
+ 输入：arr = [1,2,2,3,3,3]
+ 输出：3
+ 解释：1、2 以及 3 都是幸运数，只需要返回其中最大的 3 。
+ 
+ 
+ 示例 3：
+ 
+ 输入：arr = [2,2,2,3,3]
+ 输出：-1
+ 解释：数组中不存在幸运数。
+ 
+ 
+ 示例 4：
+ 
+ 输入：arr = [5]
+ 输出：-1
+ 
+ 
+ 示例 5：
+ 
+ 输入：arr = [7,7,7,7,7,7,7]
+ 输出：7
+ 
+ 
+ 
+ 
+ 提示：
+ 
+ 
+ 1 <= arr.length <= 500
+ 1 <= arr[i] <= 500
+ 
+ 
+ Related Topics 数组 哈希表 计数 👍 44 👎 0
+ 
+ */
+import { assertEquals } from 'https://deno.land/std@0.177.0/testing/asserts.ts'
+
+//leetcode submit region begin(Prohibit modification and deletion)
+function findLucky(arr: number[]): number {
+    const hash = new Map()
+    for (const num of arr) {
+        if (hash.has(num)) {
+            hash.set(num, hash.get(num) + 1)
+        } else {
+            hash.set(num, 1)
+        }
+    }
+    let res = -Infinity
+    for (const [num, count] of hash.entries()) {
+        if (num === count && res < num) res = num
+    }
+    return res === -Infinity ? -1 : res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+Deno.test('test', async (t) => {
+    await t.step('1,', () => {
+        assertEquals(findLucky([2, 2, 3, 4]), 2)
+    })
+    await t.step('2,', () => {
+        assertEquals(findLucky([1, 2, 2, 3, 3, 3]), 3)
+    })
+    await t.step('3,', () => {
+        assertEquals(findLucky([2, 2, 2, 3, 3]), -1)
+    })
+})
