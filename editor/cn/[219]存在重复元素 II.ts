859d5c2349c3757bbb9e6b0f6c9d0ac7da067f42
zyH <hzy18023@gmail.com>
2023-02-19 16:06:39 +0800

    feat: [219]

---
+++ editor/cn/[219]存在重复元素 II.ts
@@ -0,0 +1,53 @@
+/**
+ 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i -
+ j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ 
+ 
+ 
+ 示例 1：
+ 
+ 
+ 输入：nums = [1,2,3,1], k = 3
+ 输出：true
+ 
+ 示例 2：
+ 
+ 
+ 输入：nums = [1,0,1,1], k = 1
+ 输出：true
+ 
+ 示例 3：
+ 
+ 
+ 输入：nums = [1,2,3,1,2,3], k = 2
+ 输出：false
+ 
+ 
+ 
+ 
+ 
+ 提示：
+ 
+ 
+ 1 <= nums.length <= 10⁵
+ -10⁹ <= nums[i] <= 10⁹
+ 0 <= k <= 10⁵
+ 
+ 
+ Related Topics 数组 哈希表 滑动窗口 👍 559 👎 0
+ 
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    for (let i = 0, len = nums.length; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (nums[i] === nums[j] && Math.abs(j - i) <= k) return true
+        }
+    }
+    return false
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
