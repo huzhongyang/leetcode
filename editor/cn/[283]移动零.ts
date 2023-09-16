2cae230280aa613b8012b6f7a6ccf79aca778fd6
zyH <hzy18023@gmail.com>
2023-09-16 19:25:05 +0800

    feat: [283]

---
+++ editor/cn/[283]移动零.ts
@@ -0,0 +1,56 @@
+/**
+ 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ 
+ 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ 
+ 
+ 
+ 示例 1:
+ 
+ 
+ 输入: nums = [0,1,0,3,12]
+ 输出: [1,3,12,0,0]
+ 
+ 
+ 示例 2:
+ 
+ 
+ 输入: nums = [0]
+ 输出: [0]
+ 
+ 
+ 
+ 提示:
+ 
+ 
+ 
+ 1 <= nums.length <= 10⁴
+ -2³¹ <= nums[i] <= 2³¹ - 1
+ 
+ 
+ 
+ 
+ 进阶：你能尽量减少完成的操作次数吗？
+ 
+ Related Topics 数组 双指针 👍 2168 👎 0
+ 
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+ let left = 0, right = nums.length - 1
+ while (left < right) {
+  if (nums[left] === 0) {
+   nums.splice(left, 1)
+   nums.push(0)
+   right -= 1
+  } else {
+   left += 1
+  }
+ }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
